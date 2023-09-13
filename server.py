176adeb9f83324e17a4c0bce7f3672cfb002be6f
mua-wallace <muawallizewi@gmail.com>
2023-09-13 22:11:27 +0100

    implemented many-to-many relationship

--- server.py
+++ server.py
@@ -2,5 +2,7 @@ from flask_app import app
 
 from flask_app.controllers.burgers import burgers
 import flask_app.controllers.restauarnts
+
+
 if __name__=="__main__":
     app.run(debug=True, host='localhost', port=7100)
